67ac065686312fcd70511936b457a25b30d95f64
Pieter-Jacobs <pieternee@gmail.com>
2021-09-23 22:24:07 +0200

    Removed argparser as we'll use hydra

--- simulation/main.py
+++ simulation/main.py
@@ -2,12 +2,12 @@ from imports import *
 from classes.Election import Election
 import numpy as np
 
+
 @hydra.main(config_path="conf", config_name="config.yaml")
 def main(cfg: DictConfig):
     election = Election(cfg)
     election.count_votes()
     pass
-
 
 
 if __name__ == "__main__":
